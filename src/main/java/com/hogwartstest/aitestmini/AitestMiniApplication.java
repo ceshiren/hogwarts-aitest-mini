4124cf23b8e4fa57504115e7d861cdd79c369aa3
tlibn <1039085737@qq.com>
2020-06-12 14:37:55 +0800

    Initial commit

---
+++ src/main/java/com/hogwartstest/aitestmini/AitestMiniApplication.java
@@ -0,0 +1,13 @@
+package com.hogwartstest.aitestmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitestMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AitestMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hogwartstest/aitestmini/AitestMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogwartstest.aitestmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitestMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
